263e1c97922b8b13aecc48f077c5f4f74696f304
AkK <akkerise@gmail.com>
2019-08-21 15:59:08 +0700

    :white_check_mark:  done module create request

--- app.js
+++ app.js
@@ -20,52 +20,55 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(session({ secret: 'thanhna', resave: true, saveUninitialized: false}));
+app.use(session({secret: 'thanhna', resave: true, saveUninitialized: false}));
 app.use(flash());
 
 // mount api before csrf is appended to the app stack
 app.use('/api', api);
 
-// app.use(csrf({ cookie: true }));
-// app.use(function(req,res,next){
-//   /**
-//    * handler error: form tampered
-//    */
-  
-//   /*
-//   if (err.code !== 'EBADCSRFTOKEN') return next(err)
+// require form input send to server have an csrfToken
+app.use(csrf({cookie: true}));
+app.use(function (err, req, res, next) {
 
-//   // handle CSRF token errors here
-//   res.status(403);
-//   res.send('form tampered with');
-//   */
+    /**
+     * handler error: form tampered
+     */
 
-//   var token = req.csrfToken();
-//   res.cookie('XSRF-TOKEN', token);
-//   res.locals.csrfToken = token;
-//   next();
-// })
+    /*
+    if (err.code !== 'EBADCSRFTOKEN') return next(err)
+
+    // handle CSRF token errors here
+    res.status(403);
+    res.send('form tampered with');
+    */
+
+    var token = req.csrfToken();
+    res.cookie('XSRF-TOKEN', token);
+    res.locals.csrfToken = token;
+    next();
+})
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const routes = require(__dirname + "/routes")
 app.use(routes)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -6,10 +6,9 @@ var models = require('../../models')
 const RequestController = {
 
     async store(req, res) {
-        let [request, created] = await Services.Request.store(req.body);
-        if(!created) return new Error(`Request is exist's`);
-        let result = request.get({plain: true});
-        return res.status(200).json({result});
+        let {err, request} = await Services.Request.store(req.body);
+        if(!err.status) return res.json({err});
+        return res.json({err, request});
     },
 
 };
