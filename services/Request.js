7d7ae864a245d7ebb681c8006fcb243cc77a42ec
AkK <akkerise@gmail.com>
2019-08-21 15:59:08 +0700

    :white_check_mark:  done module create request

--- services/Request.js
+++ services/Request.js
@@ -1,10 +1,23 @@
 var models = require('../models');
 
 module.exports = {
-    
-    async store(params){
-        let {name, email, phone} = params;
-        return models.request.findOrCreate({where: {name, email, phone}, defaults: {name, email, phone}});
+
+    async store(params) {
+        let {name, email, phone, content} = params;
+        try {
+            let [request, created] = await models.request.findOrCreate({
+                where: {name, email, phone},
+                defaults: {name, email, phone, content}
+            });
+            if (!created) return {err: {status: created, message: `Request is exist's`}};
+            return {
+                err: {status: created, message: `Request create successfully!`},
+                request: request.get({plain: true})
+            };
+        } catch (err) {
+            return {err: {status: false, message: `Something error!`}};
+        }
+
     },
-    
+
 }
